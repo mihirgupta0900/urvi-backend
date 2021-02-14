feed1217ef816ff7d3833054fb62429dce04f686
Mihir Gupta <mihirgupta0900@gmail.com>
2021-02-14 15:11:24 +0530

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b696535ae80fcb24d6d662fec663f8a7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ce33d9f2-b784-474a-ba64-dcf973564367'
+};
